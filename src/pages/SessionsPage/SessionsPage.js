1c74547fed4d9be4ae23df05aab2cd11f86142f4
oliveiratullio <tullio_sfmg2@hotmail.com>
2023-03-13 15:36:35 -0300

    import axios

--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components"
+import axios from "axios";
 
 export default function  SessionsPage(props) {
     const { setMovieId } = props;
